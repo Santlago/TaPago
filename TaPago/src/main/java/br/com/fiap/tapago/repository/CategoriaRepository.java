8a1edf387ffe9093bb0f216f884c99e205727907
Santlago <breno.santiago.8@gmail.com>
2024-04-18 10:37:47 -0300

    feat: updating project

--- TaPago/src/main/java/br/com/fiap/tapago/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.tapago.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.tapago.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
-}
--- TaPago/src/main/java/br/com/fiap/tapago/repository/MovimentacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.tapago.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.tapago.model.Movimentacao;
-
-public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
-    
-}

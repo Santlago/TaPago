bf3c32aa95837a8849adddec05e508ad3cbb2769
Santlago <breno.santiago.8@gmail.com>
2024-05-06 13:30:00 -0300

    feat: updating

--- TaPago-main/src/main/java/br/com/fiap/tapago/controller/CategoriaController.java
+++ TaPago-main/src/main/java/br/com/fiap/tapago/controller/CategoriaController.java
@@ -7,6 +7,9 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,24 +24,48 @@ import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.tapago.model.Categoria;
 import br.com.fiap.tapago.repository.CategoriaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("categoria")
 @Slf4j
+@CacheConfig(cacheNames = "categorias")
+@Tag(name = "categorias")
 public class CategoriaController {
 
     @Autowired // Injeção de Dependência - Inversão de Controle
     CategoriaRepository repository;
 
     @GetMapping
+    @Cacheable
+    @Operation(
+        summary = "Listar todas as categorias",
+        description = "Retorna um array com todas as categorias no formato objeto"
+    )
     public List<Categoria> index() {
         return repository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(CREATED)
+    @CacheEvict(allEntries = true)
+    @Operation(
+        summary = "Cadastrar categoria",
+        description = "Cria uma nova categoria com os dados enviados no corpo da requisição."
+    )
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "201",description = "Categoria cadastrada com sucesso"),
+            @ApiResponse(responseCode = "400",description = "Dados enviados são inválidos. Verifique o corpo da requisição", useReturnTypeSchema = false)
+        }
+    )
     public Categoria create(@RequestBody @Valid Categoria categoria) {
         log.info("Cadastrando categoria {}", categoria);
         return repository.save(categoria);
@@ -57,13 +84,15 @@ public class CategoriaController {
 
     @DeleteMapping("{id}")
     @ResponseStatus(NO_CONTENT)
+    @CacheEvict(allEntries = true)
     public void destroy(@PathVariable Long id) {
         log.info("apagando categoria {}", id);
         verificarSeCategoriaExiste(id);
         repository.deleteById(id);
     }
-
+    
     @PutMapping("{id}")
+    @CacheEvict(allEntries = true)
     public Categoria update(@PathVariable Long id, @RequestBody Categoria categoria) {
         log.info("atualizar categoria {} para {}", id, categoria);
 
